0cc698fc0fb68083332b74e76104ebc1da53408e
Fiol Thiago <fiolthiago9@gmail.com>
2024-02-26 18:17:11 -0300

    añadimos el maquetado de el componente inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Footer from './components/common/Footer'
 import { BrowserRouter, Route, Router } from 'react-router-dom'
+import Inicio from './components/pages/Inicio'
 
 
 function App() {
   return (
     <>
+      <Inicio></Inicio>
       <Footer></Footer>
     </>
   )
---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import CardRecetas from './recetas/CardRecetas';
+
+const Inicio = () => {
+    return (
+        <section className='mainSection'>
+            <div className='tituloInicio py-4'>
+                <h2 className='text-center'>Recetas para la cocina</h2>
+            </div>
+            <Container className='my-3'>
+                <Row>
+                    <CardRecetas></CardRecetas>
+                    <CardRecetas></CardRecetas>
+                    <CardRecetas></CardRecetas>
+                    <CardRecetas></CardRecetas>
+                </Row>
+            </Container>
+        </section>
+    );
+};
+
+export default Inicio;
