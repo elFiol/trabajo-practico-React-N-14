98d99552e40e7536952c9e4fd6a347945d6eea89
JulianAballay <juliancabj1210@gmail.com>
2024-02-27 12:44:26 -0300

    modifico maquetado del formulario, agrego titulo y maquetado del componente formulario

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Footer from "./components/common/Footer";
 import { BrowserRouter, Route, Router } from "react-router-dom";
 import Inicio from "./components/pages/Inicio";
 import Menu from "./components/common/Menu";
+import FormularioReceta from "./components/pages/recetas/FormularioRecetas";
 
 
 function App() {
   return (
     <>
       <Menu></Menu>
-      <Inicio></Inicio>
+      {/* <Inicio></Inicio> */}
+      <FormularioReceta />
       <Footer></Footer>
     </>
   );
--- src/components/pages/recetas/FormularioRecetas.jsx
+++ src/components/pages/recetas/FormularioRecetas.jsx
@@ -2,38 +2,44 @@ import { Container, Form } from "react-bootstrap";
 
 const FormularioReceta = () => {
   return (
-    <section>
+    <section className="mainSection">
       <Container>
+        <h1 className="colorLabels display-2 mb-3">Formulario de recetas</h1>
         <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Nombre de la receta*</Form.Label>
+          <Form.Group className="mb-4">
+            <Form.Label className="colorLabels">
+              Nombre de la receta*
+            </Form.Label>
             <Form.Control type="text" placeholder="Ej: Fideos con salsa" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Tipo de cocina*</Form.Label>
+          <Form.Group className="mb-4">
+            <Form.Label className="colorLabels">Tipo de cocina*</Form.Label>
             <Form.Control
               type="text"
               placeholder="Ej: Italiana, Mexicana, Asiática, Mediterránea, etc."
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Ingrediente*</Form.Label>
+          <Form.Group className="mb-4">
+            <Form.Label className="colorLabels">Ingrediente*</Form.Label>
             <Form.Control
               type="text"
               placeholder="Espacio para ingresar los ingredientes, separados por comas o en líneas separadas"
             />
           </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Instrucciones* </Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Tiempo de preparación* </Form.Label>
-            <Form.Control as="textarea" rows={3} />
+
+          <Form.Group className="mb-4">
+            <Form.Label className="colorLabels">
+              Tiempo de preparación*{" "}
+            </Form.Label>
+            <Form.Control type="text" placeholder="Ej: 1 hora, 2 hora" />
           </Form.Group>
           <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label>Cargar imagen</Form.Label>
+            <Form.Label className="colorLabels">Cargar imagen</Form.Label>
             <Form.Control type="file" />
+          </Form.Group>
+          <Form.Group className="mb-4">
+            <Form.Label className="colorLabels">Instrucciones* </Form.Label>
+            <Form.Control as="textarea" rows={3} />
           </Form.Group>
         </Form>
       </Container>
