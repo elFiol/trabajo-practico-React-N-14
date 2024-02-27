6a54be82a9b72f406739788ed970615b5d989c9e
JulianAballay <juliancabj1210@gmail.com>
2024-02-27 11:30:22 -0300

    cambio el link de recetas por administrador. Elimino comentarios de codigo

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -10,19 +10,8 @@ const Menu = () => {
         <Navbar.Collapse id="basic-navbar-nav" >
           <Nav className="ms-auto">
             <Nav.Link href="#home" className='navLink'>Inicio</Nav.Link>
-            <Nav.Link href="#link" className='navLink'>Recetas</Nav.Link>
+            <Nav.Link href="#link" className='navLink'>Administrador</Nav.Link>
             <Nav.Link href="#link" className='navLink'>Nosotros</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
