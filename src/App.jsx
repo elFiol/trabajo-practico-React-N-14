6332e799ac7527d0c69aeae2aa11cbfa2a909df3
JulianAballay <juliancabj1210@gmail.com>
2024-02-26 22:36:51 -0300

    corrijo el css aplicando background al root, y min-height para bajar el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import Footer from './components/common/Footer'
-import { BrowserRouter, Route, Router } from 'react-router-dom'
-import Inicio from './components/pages/Inicio'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/common/Footer";
+import { BrowserRouter, Route, Router } from "react-router-dom";
+import Inicio from "./components/pages/Inicio";
+import Menu from "./components/common/Menu";
 
 function App() {
   return (
     <>
+      <Menu></Menu>
       <Inicio></Inicio>
       <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,31 @@
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 const Menu = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Recetas</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
