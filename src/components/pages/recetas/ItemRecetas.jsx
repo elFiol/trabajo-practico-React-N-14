c850f3de814dfa94b2d2fe494dc987c4112d9187
JulianAballay <juliancabj1210@gmail.com>
2024-02-27 23:43:30 -0300

    modifico maquetado de los campo del items

--- src/components/pages/recetas/ItemRecetas.jsx
+++ src/components/pages/recetas/ItemRecetas.jsx
@@ -2,18 +2,18 @@ const ItemRecetas = () => {
   return (
     <tr>
       <td>1</td>
-      <td>Café Americano</td>
-      <td>$250</td>
+      <td>receta 1</td>
+      <td>categoria</td>
       <td>
         <img src="" alt="" />
       </td>
       <td>Bebida caliente</td>
       <td></td>
       <td>
-        <a href="" className="btn btn-danger mx-2">
+        <a className="btn btn-danger mx-2">
           <i className="bi bi-trash"></i>
         </a>
-        <a href="" className="btn btn-warning">
+        <a className="btn btn-warning">
           <i className="bi bi-pencil-square"></i>
         </a>
       </td>
